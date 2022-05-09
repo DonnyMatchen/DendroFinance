98eefa0882cb2f89f919cbe205bcec9f472d438a
donny <donnymatchen@gmail.com>
2022-05-09 18:51:18 -0400

    budgets can now be used as templates for new budgets

--- src/com/donny/dendrofinance/gui/menu/reports/budget/NewBudgetGui.java
+++ src/com/donny/dendrofinance/gui/menu/reports/budget/NewBudgetGui.java
@@ -9,8 +9,9 @@ import javax.swing.*;
 public class NewBudgetGui extends JDialog {
     public final BudgetGui CALLER;
     private final Instance CURRENT_INSTANCE;
-    private final JLabel A;
+    private final JLabel A, B;
     private final JTextField NAME;
+    private final JComboBox<String> TEMPLATE;
     private final JButton CANCEL, OK;
 
     public NewBudgetGui(BudgetGui caller, Instance curInst) {
@@ -20,14 +21,44 @@ public class NewBudgetGui extends JDialog {
         //draw gui
         {
             A = new JLabel("Budget Name:");
+            B = new JLabel("Template");
             NAME = new JTextField();
+            TEMPLATE = new JComboBox<>();
+            TEMPLATE.addItem("Blank");
+            for (BudgetEntry entry : CURRENT_INSTANCE.DATA_HANDLER.readBudgets()) {
+                TEMPLATE.addItem(entry.getName());
+            }
             CANCEL = DendroFactory.getButton("Cancel");
             CANCEL.addActionListener(event -> dispose());
             OK = DendroFactory.getButton("Ok");
             OK.addActionListener(event -> {
-                CURRENT_INSTANCE.DATA_HANDLER.addBudget(BudgetEntry.getEmptyBudget(NAME.getText(), CURRENT_INSTANCE));
-                CALLER.updateBudget();
-                dispose();
+                boolean flag = true;
+                BudgetEntry template = null;
+                if (NAME.getText().equals("")) {
+                    flag = false;
+                }
+                if (flag) {
+                    for (BudgetEntry entry : CURRENT_INSTANCE.DATA_HANDLER.readBudgets()) {
+                        if (entry.getName().equalsIgnoreCase(NAME.getText())) {
+                            flag = false;
+                            break;
+                        }
+                        if (entry.getName().equals(TEMPLATE.getSelectedItem().toString())) {
+                            template = entry;
+                        }
+                    }
+                }
+                if (flag) {
+                    if (template == null) {
+                        CURRENT_INSTANCE.DATA_HANDLER.addBudget(new BudgetEntry(NAME.getText(), CURRENT_INSTANCE));
+                    } else {
+                        CURRENT_INSTANCE.DATA_HANDLER.addBudget(new BudgetEntry(template, NAME.getText()));
+                    }
+                    CALLER.updateBudget();
+                    dispose();
+                } else {
+                    NAME.setBackground(DendroFactory.WRONG);
+                }
             });
 
             //back layout
@@ -41,6 +72,12 @@ public class NewBudgetGui extends JDialog {
                                                 A, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE
                                         ).addGap(DendroFactory.SMALL_GAP).addComponent(
                                                 NAME, GroupLayout.PREFERRED_SIZE, 250, Short.MAX_VALUE
+                                        )
+                                ).addGroup(
+                                        main.createSequentialGroup().addComponent(
+                                                B, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE
+                                        ).addGap(DendroFactory.SMALL_GAP).addComponent(
+                                                TEMPLATE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE
                                         )
                                 ).addGroup(
                                         main.createSequentialGroup().addComponent(
@@ -59,6 +96,12 @@ public class NewBudgetGui extends JDialog {
                                         A, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE
                                 ).addComponent(
                                         NAME, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE
+                                )
+                        ).addGap(DendroFactory.SMALL_GAP).addGroup(
+                                main.createParallelGroup(GroupLayout.Alignment.CENTER).addComponent(
+                                        B, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE
+                                ).addComponent(
+                                        TEMPLATE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE
                                 )
                         ).addGap(DendroFactory.SMALL_GAP).addGroup(
                                 main.createParallelGroup(GroupLayout.Alignment.CENTER).addComponent(
